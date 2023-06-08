e9d5e2cd58f5cc0c4c0f40ee24a08e9e7df2540a
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2023-06-08 15:28:26 -0300

    [FIX] Calling from terminal.

--- python_robocode/main.py
+++ python_robocode/main.py
@@ -7,9 +7,14 @@ from PyQt5.QtWidgets import QApplication
 
 from python_robocode.GUI.window import MainWindow
 
-if __name__ == "__main__":
+
+def application():
     app = QApplication(sys.argv)
     app.setApplicationName("Python-Robocode")
     myapp = MainWindow()
     myapp.show()
     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    application()
